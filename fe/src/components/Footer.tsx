1d4d81343f82c6ef85788af34a38d570f8027745
Mariusz Klinger <mariusz@mariuszklinger.pl>
2019-12-22 10:40:22 +0100

    gitignore improved

--- fe/src/components/Footer.tsx
+++ fe/src/components/Footer.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react';
 const Footer: React.FC = () => {
   return (
     <div>
-      Powered by <a href="https://www.coindesk.com/price/bitcoin" target="_blank">CoinDesk</a> & <a href="https://openexchangerates.org" target="_blank">openexchangerates.org</a>
+      Powered by <a href="https://www.coindesk.com/price/bitcoin" target="_blank" rel="noopener noreferrer">CoinDesk</a> &
+      <a href="https://openexchangerates.org" target="_blank" rel="noopener noreferrer">openexchangerates.org</a>
     </div>
    );
 };
