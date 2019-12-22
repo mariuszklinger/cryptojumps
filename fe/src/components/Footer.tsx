b2bfd2a696f987bc76265664b9acdc52d8236291
Mariusz Klinger <mariusz@mariuszklinger.pl>
2019-12-22 10:22:34 +0100

    added footer, example data

---
+++ fe/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      Powered by <a href="https://www.coindesk.com/price/bitcoin" target="_blank">CoinDesk</a> & <a href="https://openexchangerates.org" target="_blank">openexchangerates.org</a>
+    </div>
+   );
+};
+
+export default Footer;
